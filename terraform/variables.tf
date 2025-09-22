04975932d042ec3d26b685c26b546840fd139230
Oscar <Oscar.zambrano@ionix.cl>
2025-09-22 22:31:56 +0200

    change variables terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,38 @@
-variable "project_id" { type = string }
-variable "region"     { type = string  default = "europe-west1" }
-variable "app_engine_location" { type = string default = "europe-west" }
-variable "network_name" { type = string default = "app-vpc" }
-variable "connector_name" { type = string default = "app-connector" }
-variable "connector_cidr" { type = string default = "10.8.0.0/28" }
-variable "redis_name" { type = string default = "app-redis" }
-variable "redis_size_gb" { type = number default = 1 }
-# GitHub
-variable "github_owner" { type = string } # e.g. "oscarzambrano"
-variable "github_repo"  { type = string } # e.g. "pokemon-app"
-variable "github_branch_regex" { type = string default = "^main$" }
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west1" # Redis/Conector VPC (región zonal)
+}
+
+variable "app_engine_location" {
+  type    = string
+  default = "europe-west"  # OJO: App Engine usa 'location_id' (sin dígito)
+}
+
+variable "network_name" {
+  type    = string
+  default = "app-vpc"
+}
+
+variable "connector_name" {
+  type    = string
+  default = "app-connector"
+}
+
+variable "connector_cidr" {
+  type    = string
+  default = "10.8.0.0/28"
+}
+
+variable "redis_name" {
+  type    = string
+  default = "app-redis"
+}
+
+variable "redis_size_gb" {
+  type    = number
+  default = 1
+}
